686a3c861aeb06f5b261a8c6e8b67ebd59191f82
Tomás Laus <tomaslaus99@gmail.com>
2024-04-16 09:31:03 -0300

    Auth in convex

--- convex/files.ts
+++ convex/files.ts
@@ -1,4 +1,4 @@
-import { v } from 'convex/values';
+import { ConvexError, v } from 'convex/values';
 import { mutation, query } from './_generated/server';
 
 export const createFile = mutation({
@@ -7,6 +7,12 @@ export const createFile = mutation({
   },
 
   handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new ConvexError('You must be logged in to upload a file');
+    }
+
     await ctx.db.insert('files', { name: args.name });
   },
 });
