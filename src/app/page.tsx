1f8980a63d5f6d81c98e04a3cf50704a020318e4
Tomás Laus <tomaslaus99@gmail.com>
2024-04-28 18:51:46 -0300

    Homepage styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,13 +6,11 @@ import { useOrganization, useUser } from '@clerk/nextjs';
 
 import { z } from 'zod';
 import { UploadButton } from './uppload-button';
-
+import { FileCard } from './file-card';
 
 export default function Home() {
   const organization = useOrganization();
   const user = useUser();
-
-
 
   let orgId: string | undefined = undefined;
   if (organization.isLoaded && user.isLoaded) {
@@ -23,15 +21,17 @@ export default function Home() {
 
   return (
     <main className='container mx-auto pt-12'>
-      <div className='flex justify-between items-center'>
+      <div className='flex justify-between items-center mb-8'>
         <h1 className='text-4xl font-bold'>Your Files</h1>
         <UploadButton />
       </div>
 
       {/*  */}
-      {files?.map((file) => {
-        return <div key={file._id}>{file.name}</div>;
-      })}
+      <div className='grid grid-cols-4 gap-4'>
+        {files?.map((file) => {
+          return <FileCard key={file._id} file={file} />;
+        })}
+      </div>
       {/*  */}
     </main>
   );
