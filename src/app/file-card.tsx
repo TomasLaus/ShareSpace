e325fc5d880c3eea3e7a1e7cdd679b0a6c3d66ca
Tomás Laus <tomaslaus99@gmail.com>
2024-04-29 13:09:25 -0300

    <add> Delete file (UI)

--- src/app/file-card.tsx
+++ src/app/file-card.tsx
@@ -1,3 +1,5 @@
+import { MoreVertical, TrashIcon } from 'lucide-react';
+import { Doc } from '../../convex/_generated/dataModel';
 import {
   Card,
   CardContent,
@@ -6,14 +8,79 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import { Doc } from '../../convex/_generated/dataModel';
 import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from '@/components/ui/alert-dialog';
+import { useState } from 'react';
 
-export function FileCard({file} : {file: Doc<'files'>}) {
+function FileCardActions() {
+  const [isConfirmOpen, setIsConfirmOpen] = useState(false);
+  return (
+    <>
+      <AlertDialog open={isConfirmOpen} onOpenChange={setIsConfirmOpen}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your account and remove
+              your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={() => {
+                // TODO: delete file
+              }}>
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      {/*  */}
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <MoreVertical />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem
+            onClick={() => setIsConfirmOpen(true)}
+            className='flex gap-1 text-red-600 items-center cursor-pointer'>
+            <TrashIcon className='h-4 w-4' /> Delete
+          </DropdownMenuItem>
+          <DropdownMenuSeparator />
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  );
+}
+
+export function FileCard({ file }: { file: Doc<'files'> }) {
   return (
     <Card>
-      <CardHeader>
-        <CardTitle>{file.name}</CardTitle>
+      <CardHeader className='relative'>
+        <CardTitle>{file.name} </CardTitle>
+        <div className='absolute top-2 right-2'>
+          <FileCardActions />
+        </div>
         {/* <CardDescription>Card Description</CardDescription> */}
       </CardHeader>
       <CardContent>
