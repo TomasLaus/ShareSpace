5b02679c3a1d24654ec1c8358796724895df19e5
Tomás Laus <tomaslaus99@gmail.com>
2024-05-21 15:14:09 -0300

    Adding landing page

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,20 +1,24 @@
 import { Button } from '@/components/ui/button';
-import { OrganizationSwitcher, UserButton } from '@clerk/nextjs';
+import { OrganizationSwitcher, SignedIn, UserButton, useSession } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 
 export function Header() {
+  const session = useSession();
+
   return (
-    <div className='border-b py-4 bg-gray-50'>
+    <div className='relative z-10 border-b py-4 bg-gray-50'>
       <div className='items-center container mx-auto justify-between flex'>
         <Link href={'/'} className='flex gap-2 items-center text-xl'>
           <Image alt='logo' src='/logo.png' width={45} height={45} />
           ShareSpace
         </Link>
 
-        <Button variant={'outline'}>
-          <Link href={'/dashboard/files'}>Your Files</Link>
-        </Button>
+        <SignedIn>
+          <Button variant={'outline'}>
+            <Link href={'/dashboard/files'}>Your Files</Link>
+          </Button>
+        </SignedIn>
 
         <div className='flex gap-2'>
           <OrganizationSwitcher />
