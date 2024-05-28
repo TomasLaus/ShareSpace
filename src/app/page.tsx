571056c1f69cf2ae1024ef0e5407240703359dcc
Tomas Laus <tomaslaus99@gmail.com>
2024-05-28 19:05:44 -0300

    Update landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import Link from 'next/link';
 
 export default function LandingPage() {
   return (
-    <div className='bg-white h-full'>
-      <div className='relative isolate px-6 pt-14 lg:px-8'>
+    <div className='relative isolate  bg-white px-6 py-24 sm:py-22 lg:px-8'>
+
+    <div className='bg-white flex flex-col'>
+      <div className='relative isolate px-6 pt-14 lg:px-8 flex-1'>
         <div
           className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'
           aria-hidden='true'>
@@ -17,7 +19,6 @@ export default function LandingPage() {
           />
         </div>
         <div className='mx-auto max-w-2xl py-8'>
-          <div className='hidden sm:mb-8 sm:flex sm:justify-center'></div>
           <div className='text-center'>
             <h1 className='text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl'>
               The easiest way to upload and share files with your company
@@ -50,5 +51,7 @@ export default function LandingPage() {
         </div>
       </div>
     </div>
+    </div>
+
   );
 }
